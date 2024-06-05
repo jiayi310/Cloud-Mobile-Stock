0737cf188d8d3e9d8f8a351e760caf67037b4396
YenTengSoh <130335336+YenTengSoh@users.noreply.github.com>
2024-06-05 17:58:30 +0800

    05.06.2024

--- build/app/generated/source/buildConfig/release/com/ms/mobilestock/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/ms/mobilestock/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ms.mobilestock";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "3.13.0";
+  public static final String VERSION_NAME = "1.0.0";
 }
